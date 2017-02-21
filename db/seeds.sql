727dca8a26a487b345dafc37203b11b517e029b6
Faisal N <narsi.faisal@gmail.com>
2017-02-21 17:36:56 -0600

    Burger app in progress

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO Burger (Burger) VALUES ('Cheese Burger');
+INSERT INTO Burger (Burger) VALUES ('BBQ Burger');
+INSERT INTO Burger (Burger) VALUES ('Fajita Burger');
+INSERT INTO Burger (Burger) VALUES ('DBL Meat Burger');
+INSERT INTO Burger (Burger) VALUES ('Chicken Strips Burger');
+INSERT INTO Burger (Burger) VALUES ('Veggie Burger');
